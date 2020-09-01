dd4303145acc52aba63656bd8986a99c7ecbea76
kenny-rogers <ylodonu@gmail.com>
2020-09-01 00:07:18 +0000

    add controller

---
+++ WebProject.Template/Controllers/CategoriesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebProject.Template.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        // GET
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
+    }
+}
